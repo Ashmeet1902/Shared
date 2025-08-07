dd1d8b06f1be88ef17e1b9c306589a243baec143
ashmeet1902 <Ashmeetsingh0066gmail.com>
2025-08-07 23:36:53 +0530

    latest

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+  default     = "rg-auto-docs"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+variable "locations" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
