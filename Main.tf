443cbd730de4f7bdec188e7ec89cd1d04439dd7f
Ashmeet1902 <ashmeetsingh0066@gmail.com>
2025-08-13 15:44:57 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -14,7 +14,3 @@ resource "azurerm_resource_group" "rg3" {
   name     = var.resource_group_name
   location = var.location
 }
-resource "azurerm_resource_group" "rg3" {
-  name     = var.resource_group_name
-  location = var.location
-}
